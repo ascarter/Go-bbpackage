16fd0de5242a871b7754e2fce11b1de07bf5942b
Andrew Carter <acarter@flipboard.com>
2014-06-25 00:33:49 -0700

    Run gofmt on test.go

--- Contents/Resources/test.go
+++ Contents/Resources/test.go
@@ -1,54 +1,51 @@
 // Go test
 
 /*
-  This is at test of the go syntax parser
+This is at test of the go syntax parser
 */
 
 /*
 
 Here is a function in a comment:
 func main() int {
-  fmt.Println("hello")
+fmt.Println("hello")
 }
 */
 
-int main(int argc, char *argv)
-{
-  printf("Hello");
-}
-
 package main
+
 import (
-  "fmt"
-  "github.com/user/newmath"
+	"fmt"
+	"github.com/user/newmath"
 )
 
 // Main method
 func main() {
-  fmt.Println("Hello world\"")
-  fmt.Println(`Hello world!
-  that spans more than one line`)
-  fmt.Println('l')
-  fmt.Println('\'')
+	fmt.Println("Hello world\"")
+	fmt.Println(`Hello world!
+that spans more than one line`)
+	fmt.Println('l')
+	fmt.Println('\'')
+
 }
 
 // A function
 func Sqrt(x float64) float64 {
-  z := 1.0
-  for i := 0; i < 1000; i++ {
-    z -= (z*z - x) / (2 * z)
-  }
-  return z
+	z := 1.0
+	for i := 0; i < 1000; i++ {
+		z -= (z*z - x) / (2 * z)
+	}
+	return z
 }
 
 func foo() int {
-  return 5;
+	return 5
 }
 
 func bar() {
-  return 6;
+	return 6
 }
 
 func (p *parser) init(fset *token.Fileset, filename string, src []byte) (ret int) {
-  p.file = fset.Addfile(filename, -1, len(src))
+	p.file = fset.Addfile(filename, -1, len(src))
 }
