1812c86f773c7af2d513d952875041ffba239bf0
Andrew Carter <acarter@flipboard.com>
2014-12-08 15:55:23 -0800

    Switch show results browser to optional
    * Added command line switch for showing the results browser (default is to put the results to stdout)
    * Switch documentDidSave to return results string instead of using osascript (hangs BBEdit)

--- Contents/Scripts/install.sh
+++ Contents/Scripts/install.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
 PATH="$(dirname "$0")/../Resources":$PATH
-(gorunner go install 2>&1) | awk '{print "error:" $0}' | goresults -t "go install"
+(gorunner go install 2>&1) | awk '{print "error:" $0}' | goresults -s -t "go install"
--- Contents/Scripts/vet.sh
+++ Contents/Scripts/vet.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
 PATH="$(dirname "$0")/../Resources":$PATH
-(gorunner go vet 2>&1) | awk '{print "error:" $0}' | goresults -t "go vet"
+(gorunner go vet 2>&1) | awk '{print "error:" $0}' | goresults -s -t "go vet"
