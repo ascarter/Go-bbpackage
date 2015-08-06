89730ceb7eb13e5bb7a141728a49a15d6b7f6887
Andrew Carter <acarter@flipboard.com>
2015-08-06 15:02:09 -0700

    Use gotags over ctags if installed

--- src/Scripts/tags.sh
+++ src/Scripts/tags.sh
@@ -1,18 +1,27 @@
 #!/bin/sh
 
 PATH="$(dirname "$0")/../Resources":$PATH
-CTAGS=/Applications/BBEdit.app/Contents/Helpers/ctags
-CTAGS_ARGS="--recurse --fields=+a+m+n+S --excmd=number --tag-relative=no"
-ctags_defs="$(dirname "$0")/../Resources/ctags"
 
 cd $(gorunner pwd)
 
-# Check to see if Go is missing
-$CTAGS --list-languages | grep Go > /dev/null
-if [ $? == 1 ]
-then
-	CTAGS_ARGS="--options='$ctags_defs' $CTAGS_ARGS"
+GOTAGS=$(gorunner which gotags)
+
+# Prefer gotags if it is installed
+if [ -z "${GOTAGS}" ]; then
+	# Use ctags
+	CTAGS=/Applications/BBEdit.app/Contents/Helpers/ctags
+	CTAGS_ARGS="--recurse --fields=+a+m+n+S --excmd=number --tag-relative=no"
+	CTAGS_DEFS="$(dirname "$0")/../Resources/ctags"
+
+	# Check to see if Go is missing
+	$CTAGS --list-languages | grep Go > /dev/null
+	if [ $? == 1 ]
+	then
+		CTAGS_ARGS="--options='$CTAGS_DEFS' $CTAGS_ARGS"
+	fi
+
+	$CTAGS $CTAGS_ARGS .
+else
+	# Use gotags
+	${GOTAGS} -f="tags" -R .
 fi
-
-$CTAGS $CTAGS_ARGS .
-
