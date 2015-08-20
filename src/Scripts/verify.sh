ab4772c040821fe6e3c2bc160ee883d6fe16d10a
Andrew Carter <ascarter@icloud.com>
2015-08-20 09:47:26 -0700

    Update verify
    Verify is a meta command that blends imports, lint, vet, and build into one command. The idea is to "check" or "verify" the current file (like a syntax check). This will likely move to a more accurate command in the future

--- src/Scripts/verify.sh
+++ src/Scripts/verify.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-PATH="$(dirname "$0")/../Resources":$PATH
+PATH="$(dirname "$0")/../Resources":"$(dirname "$0")":$PATH
 
 # Expect BB_DOC_LANGUAGE, BB_DOC_MODE, BB_DOC_NAME, BB_DOC_PATH
 # On save, run goimports, golint, go vet
 (
-	(gorunner goimports -w "$BB_DOC_PATH" 2>&1)
-	(gorunner golint "$BB_DOC_PATH" 2>&1) | awk '{print "warning:" $0}'
-	(gorunner go vet 2>&1)
-	(gorunner go build $(gorunner go list) 2>&1)
+    goimports.sh
+    golint.sh
+    vet.sh
+    build.sh
 ) | bbresults -t "Verify"
