ed2342ba0327372b5149f1e7afe5a71524b93361
Andrew Carter <ascarter@icloud.com>
2015-08-25 09:25:59 -0700

    Add godef script

---
+++ src/Scripts/godef.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+PATH="$(dirname "$0")/../Resources":$PATH
+def=$(gorunner godef -t -f "$BB_DOC_PATH" -o $BB_DOC_SELSTART)
+osascript - "$def" <<EOF
+on run argv
+  display dialog argv
+end run
+EOF
