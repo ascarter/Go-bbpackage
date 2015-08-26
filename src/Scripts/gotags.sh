f4078cb33882e75e132c9dc0e807e7409623a0e5
Andrew Carter <acarter@flipboard.com>
2015-08-26 16:17:54 -0700

    Revise gotags to use git root
    Uses `git rev-parse` to find the root of the project and runs the tags against it.
    
    TODO: fall back to using either the project or document path if git root not found

--- src/Scripts/gotags.sh
+++ src/Scripts/gotags.sh
@@ -2,16 +2,18 @@
 
 PATH="$(dirname "$0")/../Resources":$PATH
 
-cd $(gorunner pwd)
-
+CTAGS=/Applications/BBEdit.app/Contents/Helpers/ctags
+CTAGS_ARGS="--recurse --fields=+a+m+n+S --excmd=number --tag-relative=no"
+CTAGS_DEFS="$(dirname "$0")/../Resources/ctags"
 GOTAGS=$(gorunner which gotags)
+
+# TODO: test to see if git rev-parse failed or not and fall back to doc path
+PROJ_DIR=$(gorunner git rev-parse --show-toplevel)
+TAGFILE="${PROJ_DIR}/tags"
 
 # Prefer gotags if it is installed
 if [ -z "${GOTAGS}" ]; then
 	# Use ctags
-	CTAGS=/Applications/BBEdit.app/Contents/Helpers/ctags
-	CTAGS_ARGS="--recurse --fields=+a+m+n+S --excmd=number --tag-relative=no"
-	CTAGS_DEFS="$(dirname "$0")/../Resources/ctags"
 
 	# Check to see if Go is missing
 	$CTAGS --list-languages | grep Go > /dev/null
@@ -20,8 +22,8 @@ if [ -z "${GOTAGS}" ]; then
 		CTAGS_ARGS="--options='$CTAGS_DEFS' $CTAGS_ARGS"
 	fi
 
-	$CTAGS $CTAGS_ARGS .
+	$CTAGS $CTAGS_ARGS -f="$TAGFILE" "$PROJ_DIR"
 else
 	# Use gotags
-	${GOTAGS} -f="tags" -R .
+	${GOTAGS} -f="$TAGFILE" -R "$PROJ_DIR"
 fi
