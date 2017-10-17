9be654c44c655fd000650c23ae421808d94a9bac
Andrew Carter <acarter@flipboard.com>
2017-10-17 14:06:28 -0700

    Revise script layout

--- src/Scripts/go build.sh
+++ src/Scripts/go build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-PATH="$(dirname "$0")/../../Resources":$PATH
+PATH="$(dirname "$0")/../Resources":$PATH
 (gorunner go build $(gorunner go list) 2>&1) | bbr -t "go build"
---
+++ src/Scripts/go run.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+PATH="$(dirname "$0")/../Resources":$PATH
+gorunner go run "$BB_DOC_PATH"
